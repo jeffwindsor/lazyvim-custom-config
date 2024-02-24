a6131d49e30234a7296b652e8bfa2207a0024690
Jeff Windsor <jeff.windsor@gmail.com>
2024-02-24 11:43:19 -0800

    initial personalization

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,22 @@
+-- Configuration File to override lazynvim options
+--
+return {
+
+  -- colorschemes
+  { "joshdick/onedark.vim" },
+  { "marko-cerovac/material.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "tomasr/molokai" },
+  { "catppuccin/nvim" },
+  { "NLKNguyen/papercolor-theme" },
+  { "jacoborus/tender.vim" },
+  { "cocopon/iceberg.vim" },
+  { "romainl/Apprentice" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
